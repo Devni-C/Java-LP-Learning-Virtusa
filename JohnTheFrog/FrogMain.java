9cbd561337fa26b06292ac31435347f5ca161d90
Devni-C <devniperera14@gmail.com>
2020-01-16 05:07:31 +0530

    Add changes to Main method functionality

--- JohnTheFrog/FrogMain.java
+++ JohnTheFrog/FrogMain.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class FrogMain {
 	public static void main(String[] args) {
-
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Press 1 to enter jumped Distance\nPress 2 to enter Time taken");
+		int input = scanner.nextInt();
+		int time, distance;
+		
+		FrogActions actions = new FrogActions();
+	
+		switch (input) {
+			case 1:
+				System.out.println("Enter the distance John jumped: ");
+				distance = scanner.nextInt();
+				actions.calculateTime(distance);
+				break;
+			case 2:
+				System.out.println("Enter the time John took to jump: ");
+				time = scanner.nextInt();
+				actions.calculateDistance(time);
+			
+				break;
+			default:
+				System.out.println("Invalid Input!");
+				break;
+		}
 	}	
 }
