5f3388143f077abcad0055b6b211b5115d2ca967
Devni-C <devniperera14@gmail.com>
2020-01-16 02:13:32 +0530

    Add full diamond star pattern

--- ForLoopPatternDemo/DiamondStarPattern/DiamondStarPattern.java
+++ ForLoopPatternDemo/DiamondStarPattern/DiamondStarPattern.java
@@ -1,20 +1,23 @@
 public class DiamondStarPattern {
 	public static void main(String[] args) {
-		for (int a = 1; a <= 4; a++) {
-			for (int i = 4; i > a; i--) {
+		int rows = 19;
+
+		for (int x = 1; x <= rows; x++) {
+		//for (int a = 1; a <= 4; a++) {
+			for (int i = rows; i > x; i--) {
         			System.out.print(" ");
 			}
-        		for (int j = 1; j <= a; j++) {
+        		for (int j = 1; j <= (x * 2)-1; j++) {
              			System.out.print("*");
             		}
             		System.out.println();
         	}
 
-		for (int a = 1; a <= 4; a++) {
-			for (int i = 1; i < a; i++) {
+		for (int y = rows - 1; y >= 1; y--) {
+			for (int i = rows - 1; i >= y; i--) {
         			System.out.print(" ");
 			}
-        		for (int j = 4; j >= a; j--) {
+        		for (int j = 1; j <= (y * 2)-1; j++) {
              			System.out.print("*");
             		}
             		System.out.println();
