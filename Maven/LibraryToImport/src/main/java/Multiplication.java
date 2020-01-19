560a67a800fc608e9c0ad362198dcff80f9173cf
Devni Perera <devniperera14@gmail.com>
2020-01-20 05:19:38 +0530

    Add file to demonstrate using Maven projects

---
+++ Maven/LibraryToImport/src/main/java/Multiplication.java
@@ -0,0 +1,6 @@
+public class Multiplication {
+    public int getMultiplication(int num1, int num2) {
+        int answer = num1 * num2;
+        return answer;
+    }
+}
---
+++ Maven/MavenLibraryDemo/src/main/java/MavenDemo.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class MavenDemo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num1, num2;
+
+        System.out.println("Enter two numbers: ");
+        num1 = scanner.nextInt();
+        num2 = scanner.nextInt();
+
+        System.out.println("Sum is: " + getSum(num1, num2));
+        System.out.println("Multiplication is: ");
+    }
+
+    static int getSum(int num1, int num2) {
+        int sum = num1 + num2;
+        return sum;
+    }
+}
