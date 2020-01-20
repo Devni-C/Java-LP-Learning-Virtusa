0e597fe1cd509f0d4a7a3107ebcefd0b47642186
Devni Perera <devniperera14@gmail.com>
2020-01-20 06:08:48 +0530

    Add initial commit

---
+++ WeightsProblem/WeightsProblem.java
@@ -0,0 +1,8 @@
+public class WeightsProblem {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter a number between 1-40");
+    int input = scanner.nextInt();
+
+  }
+}
