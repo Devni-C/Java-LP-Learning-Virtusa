f670a57413863878bb56a2cb9e626ce238216d4f
Devni Perera <devniperera14@gmail.com>
2020-01-20 05:47:27 +0530

    Add minor changes of MavenDemo.java

--- Maven/MavenLibraryDemo/src/main/java/MavenDemo.java
+++ Maven/MavenLibraryDemo/src/main/java/MavenDemo.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class MavenDemo {
     public static void main(String[] args) {
+        Multiplication obj = new Multiplication();
         Scanner scanner = new Scanner(System.in);
         int num1, num2;
 
@@ -10,7 +11,7 @@ public class MavenDemo {
         num2 = scanner.nextInt();
 
         System.out.println("Sum is: " + getSum(num1, num2));
-        System.out.println("Multiplication is: ");
+        System.out.println("Multiplication is: " + obj.getMultiplication(num1, num2));
     }
 
     static int getSum(int num1, int num2) {
