b9f71aa11c18d5342b70114d5bbf42c551767bab
Devni Perera <devniperera14@gmail.com>
2020-03-02 04:58:31 +0530

    Add RentCloudCommons model files

---
+++ MicroServicesPractice/RentCloudCommons/src/main/java/com/devni/tlp/rentcloud/commons/model/Customer.java
@@ -0,0 +1,58 @@
+package com.devni.tlp.rentcloud.commons.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "customer")
+public class Customer {
+    @Id
+            @GeneratedValue
+    int id;
+    String firstName;
+    String lastName;
+    String dlNumber;
+    String zipCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDlNumber() {
+        return dlNumber;
+    }
+
+    public void setDlNumber(String dlNumber) {
+        this.dlNumber = dlNumber;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+}
