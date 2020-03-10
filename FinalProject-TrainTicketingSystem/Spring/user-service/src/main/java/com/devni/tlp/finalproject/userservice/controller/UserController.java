e6f155583c7462c967b1d1195561caf8f8dc932c
Devni-C <devniperera14@gmail.com>
2020-03-10 17:59:49 +0530

    Add minor changes to user-service

--- FinalProject-TrainTicketingSystem/Spring/user-service/src/main/java/com/devni/tlp/finalproject/userservice/controller/UserController.java
+++ FinalProject-TrainTicketingSystem/Spring/user-service/src/main/java/com/devni/tlp/finalproject/userservice/controller/UserController.java
@@ -1,7 +1,13 @@
 package com.devni.tlp.finalproject.userservice.controller;
 
+import com.devni.tlp.finalproject.userservice.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/user")
 public class UserController {
+    @Autowired
+    UserService userService;
 }
--- FinalProject-TrainTicketingSystem/Spring/user-service/src/main/java/com/devni/tlp/finalproject/userservice/service/UserServiceImpl.java
+++ FinalProject-TrainTicketingSystem/Spring/user-service/src/main/java/com/devni/tlp/finalproject/userservice/service/UserServiceImpl.java
@@ -1,9 +1,13 @@
 package com.devni.tlp.finalproject.userservice.service;
 
+import com.devni.tlp.finalproject.userservice.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Service
 @RequestMapping("/service")
 public class UserServiceImpl implements UserService {
+    @Autowired
+    UserRepository userRepository;
 }
