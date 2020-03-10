cf369aad9ac1d105aad03745ae4a7eb35d6e52ac
Devni Perera <devniperera14@gmail.com>
2020-03-10 06:37:17 +0530

    Add minor changes for route-service models

--- FinalProject-TrainTicketingSystem/Spring/route-service/src/main/java/com/devni/tlp/finalproject/routeservice/model/Station.java
+++ FinalProject-TrainTicketingSystem/Spring/route-service/src/main/java/com/devni/tlp/finalproject/routeservice/model/Station.java
@@ -3,12 +3,19 @@ package com.devni.tlp.finalproject.routeservice.model;
 import lombok.Data;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 @Data
 public class Station {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
     private String location;
     private String telephone;
+
+    Train train;
 }
--- FinalProject-TrainTicketingSystem/Spring/route-service/src/main/java/com/devni/tlp/finalproject/routeservice/model/Train.java
+++ FinalProject-TrainTicketingSystem/Spring/route-service/src/main/java/com/devni/tlp/finalproject/routeservice/model/Train.java
@@ -3,13 +3,23 @@ package com.devni.tlp.finalproject.routeservice.model;
 import lombok.Data;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.Instant;
 
 @Entity
 @Data
 public class Train {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
-    private int seat;
+    private static int seat;
+    private Instant startTime;
+    private Instant endTime;
+    private Station startStation;
+    private Station endStation;
 
     enum tClass {
         FIRST_CLASS,
