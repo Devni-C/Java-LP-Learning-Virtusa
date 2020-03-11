e5c269fe98fd6d0e08a04d2e89f33f559af2974d
Devni-C <devniperera14@gmail.com>
2020-03-11 17:35:20 +0530

    Add minor changes to user-service

--- FinalProject-LibraryManagementSystem/Spring/user-service/src/main/java/com/devni/tlp/finalproject/userservice/model/User.java
+++ FinalProject-LibraryManagementSystem/Spring/user-service/src/main/java/com/devni/tlp/finalproject/userservice/model/User.java
@@ -3,13 +3,18 @@ package com.devni.tlp.finalproject.userservice.model;
 import lombok.Data;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 @Data
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Integer id;
+
     String name;
     String NIC;
     String mobile;
-//    String
 }
