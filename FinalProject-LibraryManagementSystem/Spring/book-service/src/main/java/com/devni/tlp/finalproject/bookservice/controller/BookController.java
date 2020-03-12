1d0db5adb9c0aa009232047d2ad1994aef905b8f
Devni-C <devniperera14@gmail.com>
2020-03-12 17:45:25 +0530

    Add methods to BookController

--- FinalProject-LibraryManagementSystem/Spring/book-service/src/main/java/com/devni/tlp/finalproject/bookservice/controller/BookController.java
+++ FinalProject-LibraryManagementSystem/Spring/book-service/src/main/java/com/devni/tlp/finalproject/bookservice/controller/BookController.java
@@ -3,10 +3,7 @@ package com.devni.tlp.finalproject.bookservice.controller;
 import com.devni.tlp.finalproject.bookservice.model.Book;
 import com.devni.tlp.finalproject.bookservice.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,7 +13,7 @@ public class BookController {
     @Autowired
     BookService bookService;
 
-    @RequestMapping("/save")
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
     public Book save(@RequestBody Book book) {
         return bookService.saveBook(book);
     }
