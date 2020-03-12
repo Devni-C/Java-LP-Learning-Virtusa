1c55a977fdccc379f10c444861a5a86e8721d470
Devni-C <devniperera14@gmail.com>
2020-03-12 17:53:00 +0530

    Add minor changes to ReservationController

--- FinalProject-LibraryManagementSystem/Spring/reservation-service/src/main/java/com/devni/tlp/finalproject/reservationservice/controller/ReservationController.java
+++ FinalProject-LibraryManagementSystem/Spring/reservation-service/src/main/java/com/devni/tlp/finalproject/reservationservice/controller/ReservationController.java
@@ -1,9 +1,12 @@
 package com.devni.tlp.finalproject.reservationservice.controller;
 
+import com.devni.tlp.finalproject.reservationservice.model.Reservation;
 import com.devni.tlp.finalproject.reservationservice.repository.ReservationRepository;
 import com.devni.tlp.finalproject.reservationservice.service.ReservationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,6 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class ReservationController {
     @Autowired
     ReservationService reservationService;
+
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public Reservation save(@RequestBody Reservation reservation) {
+        return reservationService.saveReservation(reservation);
+    }
+
 
 //    isAvailable,
 }
