bac87e0abd456279c304f2947f0d3d8ae8be10ec
Devni-C <devniperera14@gmail.com>
2020-03-12 17:59:35 +0530

    Add minor changes to User Service

--- FinalProject-LibraryManagementSystem/Spring/user-service/src/main/java/com/devni/tlp/finalproject/userservice/service/UserService.java
+++ FinalProject-LibraryManagementSystem/Spring/user-service/src/main/java/com/devni/tlp/finalproject/userservice/service/UserService.java
@@ -1,4 +1,13 @@
 package com.devni.tlp.finalproject.userservice.service;
 
+import com.devni.tlp.finalproject.userservice.model.User;
+
+import java.util.List;
+
 public interface UserService {
+    User saveUser(User user);
+
+    List<User> fetchAllUsers();
+
+    User fetchUserById(int id);
 }
--- FinalProject-LibraryManagementSystem/Spring/user-service/src/main/java/com/devni/tlp/finalproject/userservice/service/UserServiceImpl.java
+++ FinalProject-LibraryManagementSystem/Spring/user-service/src/main/java/com/devni/tlp/finalproject/userservice/service/UserServiceImpl.java
@@ -1,11 +1,31 @@
 package com.devni.tlp.finalproject.userservice.service;
 
+import com.devni.tlp.finalproject.userservice.model.User;
 import com.devni.tlp.finalproject.userservice.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService {
     @Autowired
     UserRepository userRepository;
+
+    @Override
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public List<User> fetchAllUsers() {
+        return  userRepository.findAll();
+    }
+
+    @Override
+    public User fetchUserById(int id) {
+        return userRepository.findById(id).get();
+    }
+
+
 }
