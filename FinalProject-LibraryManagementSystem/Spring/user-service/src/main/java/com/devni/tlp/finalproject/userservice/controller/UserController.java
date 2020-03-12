e43b343608a0df15e63c830058f0b3c52db41099
Devni-C <devniperera14@gmail.com>
2020-03-12 17:58:50 +0530

    Add minor changes to UserController

--- FinalProject-LibraryManagementSystem/Spring/user-service/src/main/java/com/devni/tlp/finalproject/userservice/controller/UserController.java
+++ FinalProject-LibraryManagementSystem/Spring/user-service/src/main/java/com/devni/tlp/finalproject/userservice/controller/UserController.java
@@ -1,13 +1,35 @@
 package com.devni.tlp.finalproject.userservice.controller;
 
+import com.devni.tlp.finalproject.userservice.model.User;
 import com.devni.tlp.finalproject.userservice.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
     @Autowired
     UserService userService;
+
+    @RequestMapping("/test")
+    public String test(){
+        return "<h1>Hi Devni :D</h1>";
+    }
+
+    @RequestMapping("/getallusers")
+    public List<User> getAllUsers() {
+        return userService.fetchAllUsers();
+    }
+
+    @RequestMapping("/getuserbyid/{uid}")
+    public User getUserById(@PathVariable int uid){
+        return userService.fetchUserById(uid);
+    }
+
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public User save(@RequestBody User user) {
+        return userService.saveUser(user);
+    }
 }
