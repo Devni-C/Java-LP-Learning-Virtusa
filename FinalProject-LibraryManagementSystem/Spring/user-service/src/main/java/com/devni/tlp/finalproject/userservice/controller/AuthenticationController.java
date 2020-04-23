c602c91544776e19af6e05bdaec127bd9bd45844
Devni Perera <devniperera14@gmail.com>
2020-04-23 12:08:04 +0530

    Fix role not saving bug

--- FinalProject-LibraryManagementSystem/Spring/user-service/src/main/java/com/devni/tlp/finalproject/userservice/controller/AuthenticationController.java
+++ FinalProject-LibraryManagementSystem/Spring/user-service/src/main/java/com/devni/tlp/finalproject/userservice/controller/AuthenticationController.java
@@ -1,57 +1,117 @@
 package com.devni.tlp.finalproject.userservice.controller;
 
-import com.devni.tlp.finalproject.userservice.config.JwtTokenUtil;
 //import com.devni.tlp.finalproject.userservice.exception.UserExistException;
-import com.devni.tlp.finalproject.userservice.model.JwtRequest;
-import com.devni.tlp.finalproject.userservice.model.JwtResponse;
-import com.devni.tlp.finalproject.userservice.model.UserDTO;
-import com.devni.tlp.finalproject.userservice.service.UserServiceImpl;
+
+import com.devni.tlp.finalproject.userservice.config.JwtTokenUtil;
+import com.devni.tlp.finalproject.userservice.exception.RoleNotFoundException;
+import com.devni.tlp.finalproject.userservice.model.*;
+import com.devni.tlp.finalproject.userservice.repository.RoleRepository;
+import com.devni.tlp.finalproject.userservice.repository.UserRepository;
+import com.devni.tlp.finalproject.userservice.service.CustomUserDetailsImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "*", maxAge = 3600)
+import javax.validation.Valid;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
+@RequestMapping("/api/auth")
 public class AuthenticationController {
     @Autowired
-    private AuthenticationManager authenticationManager;
+    AuthenticationManager authenticationManager;
 
     @Autowired
-    private JwtTokenUtil jwtTokenUtil;
+    UserRepository userRepository;
 
     @Autowired
-    private UserServiceImpl jwtUserDetailsService;
+    RoleRepository roleRepository;
 
-    @RequestMapping(value = "/authenticate", method = RequestMethod.POST)
-    public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtRequest authenticationRequest) throws Exception {
+    @Autowired
+    PasswordEncoder encoder;
 
-        authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());
+    @Autowired
+    JwtTokenUtil jwtUtils;
 
-        final UserDetails userDetails = jwtUserDetailsService
-                .loadUserByUsername(authenticationRequest.getUsername());
+    @PostMapping("/signin")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody JwtRequest loginRequest) {
 
-        final String token = jwtTokenUtil.generateToken(userDetails);
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
 
-        return ResponseEntity.ok(new JwtResponse(token));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = jwtUtils.generateJwtToken(authentication);
+
+        CustomUserDetailsImpl userDetails = (CustomUserDetailsImpl) authentication.getPrincipal();
+        List<String> roles = userDetails.getAuthorities()
+                .stream()
+                .map(item -> item.getAuthority())
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(new JwtResponse(jwt,
+                userDetails.getId(),
+                userDetails.getUsername(),
+                userDetails.getEmail(),
+                roles));
     }
 
-    private void authenticate(String username, String password) throws Exception {
-        try {
-            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
-        } catch (DisabledException e) {
-            throw new Exception("USER_DISABLED", e);
-        } catch (BadCredentialsException e) {
-            throw new Exception("INVALID_CREDENTIALS", e);
+    @PostMapping("/signup")
+    public ResponseEntity<?> registerUser(@Valid @RequestBody UserDTO signUpRequest) {
+        if (userRepository.existsByUsername(signUpRequest.getUsername())) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(new MessageResponse("Error: Username is already taken!"));
         }
-    }
 
-    @RequestMapping(value = "/register", method = RequestMethod.POST)
-    public ResponseEntity<?> saveUser(@RequestBody UserDTO user) throws Exception {
-        return ResponseEntity.ok(jwtUserDetailsService.save(user));
+        if (userRepository.existsByEmail(signUpRequest.getEmail())) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(new MessageResponse("Error: Email is already in use!"));
+        }
+
+        // Create new user's account
+        User user = new User(signUpRequest.getUsername(),
+                signUpRequest.getEmail(),
+                encoder.encode(signUpRequest.getPassword()));
+
+        Set<String> strRoles = signUpRequest.getRole();
+        Set<Role> roles = new HashSet<>();
+
+        if (strRoles.size() == 0) {
+            Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+                    .orElseThrow(() -> new RoleNotFoundException("Error: Role is not found."));
+            roles.add(userRole);
+        } else {
+            strRoles.forEach(role -> {
+                if (role.toLowerCase().equals("admin")) {
+                    Role adminRole = roleRepository.findByName(ERole.ROLE_ADMIN)
+                            .orElseThrow(() -> new RoleNotFoundException("Error: Role is not found."));
+                    roles.add(adminRole);
+                } else if (role.toLowerCase().equals("user")) {
+                    Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+                            .orElseThrow(() -> new RoleNotFoundException("Error: Role is not found."));
+                    roles.add(userRole);
+                } else {
+                    Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+                            .orElseThrow(() -> new RoleNotFoundException("Error: Role is not found."));
+                    roles.add(userRole);
+                }
+            });
+        }
+
+        user.setRoles(roles);
+        System.out.println("roles: " + roles.size());
+        userRepository.save(user);
+
+        return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
     }
 }
