6588c544d7919f254d6fd7befdf3d5b1f88a3b38
Devni Perera <devniperera14@gmail.com>
2020-04-27 23:24:43 +0530

    Add changes to book component

--- FinalProject-LibraryManagementSystem/Angular/library-mgt-system/src/app/components/admin/books/books.component.ts
+++ FinalProject-LibraryManagementSystem/Angular/library-mgt-system/src/app/components/admin/books/books.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Book} from '../../../models/Book';
+import {BookService} from '../../../services/book/book.service';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-books',
@@ -7,9 +10,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BooksComponent implements OnInit {
 
-  constructor() { }
+  books: Array<Book>;
+  booksReceived: Array<Book>;
+  selectedBook: Book;
+  action: string;
+
+  constructor(private bookService: BookService,
+              private activatedRoute: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit(): void {
+    this.refreshData();
   }
 
+  refreshData() {
+    this.bookService.getBooks().subscribe(
+      response => this.handleSuccessfulResponse(response)
+    );
+
+    this.activatedRoute.queryParams.subscribe(
+      (params) => {
+        this.action = params.action;
+        const id = params.id;
+        if (id) { // show the view page for the selected user
+          this.selectedBook = this.books.find(book => {
+            return book.id === +id;
+          });
+        }
+      }
+    );
+  }
+
+  handleSuccessfulResponse(response) {
+    this.books = new Array<Book>();
+    this.booksReceived = response;
+
+    for (const book of this.booksReceived) {
+      const bookWithRetrivedImageField = new Book();
+      bookWithRetrivedImageField.id = book.id;
+      bookWithRetrivedImageField.title = book.title;
+
+      bookWithRetrivedImageField.retrievedImage = 'data:image/jpeg;base64,' + book.picByte;
+      bookWithRetrivedImageField.authors = book.authors;
+      bookWithRetrivedImageField.isbn = book.isbn;
+      // bookWithRetrivedImageField.authors = book.authors;
+      this.books.push(bookWithRetrivedImageField);
+    }
+  }
+
+  addBook() {
+    this.selectedBook = new Book();
+    this.router.navigate(['books'], {queryParams: {action: 'add'}});
+  }
+
+  viewBook(id: number) {
+    this.router.navigate(['books'], {queryParams: {id, action: 'view'}});
+  }
 }
