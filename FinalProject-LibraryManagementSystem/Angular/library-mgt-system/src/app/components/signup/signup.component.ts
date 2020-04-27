2a019cfd0d0dd3f74f6140e8cb2fb6b06e4d26ba
Devni Perera <devniperera14@gmail.com>
2020-04-27 23:31:55 +0530

    Add UI and logic modifications for signup component

--- FinalProject-LibraryManagementSystem/Angular/library-mgt-system/src/app/components/signup/signup.component.ts
+++ FinalProject-LibraryManagementSystem/Angular/library-mgt-system/src/app/components/signup/signup.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from '../../services/auth/auth.service';
+import {findLast} from '@angular/compiler/src/directive_resolver';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+// import {ConfirmedValidator} from './confirmed.validator';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +10,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
+  form: any = {};
+  isSuccessful = false;
+  isSignUpFailed = false;
+  errorMessage = '';
 
-  constructor() { }
+  constructor(private authService: AuthService) {
+  }
 
   ngOnInit(): void {
   }
 
+  onSubmit() {
+    console.log(this.form.value);
+    this.authService.register(this.form).subscribe(
+      data => {
+        console.log('data ' + data);
+        this.isSuccessful = true;
+        this.isSignUpFailed = false;
+      },
+      err => {
+        this.errorMessage = err.error.message;
+        this.isSignUpFailed = true;
+      }
+    );
+  }
 }
