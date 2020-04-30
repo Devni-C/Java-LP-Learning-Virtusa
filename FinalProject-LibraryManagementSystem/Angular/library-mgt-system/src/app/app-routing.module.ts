9bed6289e5834ce7ed185f606c3e9487280dad72
Devni Perera <devniperera14@gmail.com>
2020-04-30 12:09:12 +0530

    Add minor changes

--- FinalProject-LibraryManagementSystem/Angular/library-mgt-system/src/app/app-routing.module.ts
+++ FinalProject-LibraryManagementSystem/Angular/library-mgt-system/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {HomeComponent} from './home/home.component';
-import {LoginComponent} from './login/login.component';
-import {SignupComponent} from './signup/signup.component';
-import {BooksComponent} from './books/books.component';
-import {UsersComponent} from './users/users.component';
+import {HomeComponent} from './components/home/home.component';
+import {LoginComponent} from './components/login/login.component';
+import {SignupComponent} from './components/signup/signup.component';
+import {BooksComponent} from './components/admin/books/books.component';
+import {UsersComponent} from './components/admin/users/users.component';
+import {ViewBooksComponent} from './components/admin/books/view-books/view-books.component';
+import {AddBookComponent} from './components/admin/books/add-book/add-book.component';
+import {ProfileComponent} from './components/profile/profile.component';
+import {ReservationComponent} from './components/reservation/reservation.component';
 
 
 const routes: Routes = [
@@ -13,7 +17,11 @@ const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'signup', component: SignupComponent},
   {path: 'books', component: BooksComponent},
-  {path: 'users', component: UsersComponent}
+  {path: 'users', component: UsersComponent},
+  {path: 'view-books', component: ViewBooksComponent},
+  {path: 'add-books', component: AddBookComponent},
+  {path: 'profile', component: ProfileComponent},
+  {path: 'reserve', component: ReservationComponent}
 ];
 
 @NgModule({
--- FinalProject-LibraryManagementSystem/Angular/library-mgt-system/src/app/app.module.ts
+++ FinalProject-LibraryManagementSystem/Angular/library-mgt-system/src/app/app.module.ts
@@ -1,14 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import { SignupComponent } from './signup/signup.component';
-import { BooksComponent } from './books/books.component';
-import { UsersComponent } from './users/users.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './components/home/home.component';
+import {LoginComponent} from './components/login/login.component';
+import {SignupComponent} from './components/signup/signup.component';
+import {BooksComponent} from './components/admin/books/books.component';
+import {UsersComponent} from './components/admin/users/users.component';
+import {NavbarComponent} from './components/navbar/navbar.component';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
+import {AddUserComponent} from './components/admin/users/add-user/add-user.component';
+import {ViewUsersComponent} from './components/admin/users/view-users/view-users.component';
+import {AddBookComponent} from './components/admin/books/add-book/add-book.component';
+import {ViewBooksComponent} from './components/admin/books/view-books/view-books.component';
+import {FooterComponent} from './components/footer/footer.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { ReservationComponent } from './components/reservation/reservation.component';
+import { LendingComponent } from './components/lending/lending.component';
 
 @NgModule({
   declarations: [
@@ -18,13 +28,24 @@ import { NavbarComponent } from './navbar/navbar.component';
     SignupComponent,
     BooksComponent,
     UsersComponent,
-    NavbarComponent
+    NavbarComponent,
+    AddUserComponent,
+    ViewUsersComponent,
+    AddBookComponent,
+    ViewBooksComponent,
+    FooterComponent,
+    ProfileComponent,
+    ReservationComponent,
+    LendingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
